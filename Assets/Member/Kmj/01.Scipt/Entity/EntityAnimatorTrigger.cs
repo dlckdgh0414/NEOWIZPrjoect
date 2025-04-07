873366e5363077dba46c3cafe664869e5b48131c
dlckdgh0414 <ci5976547@gmail.com>
2025-04-07 16:18:21 +0900

    fix : Animtrigger

--- Assets/Member/Kmj/01.Scipt/Entity/EntityAnimatorTrigger.cs
+++ Assets/Member/Kmj/01.Scipt/Entity/EntityAnimatorTrigger.cs
@@ -7,6 +7,16 @@ public class EntityAnimatorTrigger : MonoBehaviour, IEntityComponet
     public Action OnAnimationEndTrigger;
     public Action OnAttackTrigger;
 
+    public Action OnAttackTriggerEnd;
+
+    public event Action OnAttackVFXTrigger;
+
+    public event Action OnStrongAttackTrigger;
+
+    public event Action OnBarrierPressed;
+
+    public event Action<bool> OnRollingStatusChange;
+
     private Entity _entity;
 
     public void Initialize(Entity entity)
@@ -18,6 +28,17 @@ public class EntityAnimatorTrigger : MonoBehaviour, IEntityComponet
     {
         OnAnimationEndTrigger?.Invoke();
     }
+    private void RollingStart() => OnRollingStatusChange?.Invoke(true);
+    private void RollingEnd() => OnRollingStatusChange?.Invoke(true);
+
+    private void PlayAttackVFX() => OnAttackVFXTrigger?.Invoke();
+
+    private void PlayerStrongAttack() => OnStrongAttackTrigger?.Invoke();
+    private void AttackEnd() => OnAttackTriggerEnd?.Invoke();
+
+
+    private void BarrierPressed() => OnBarrierPressed?.Invoke();
+
 
     private void Attack()
     {
