4401ff4ef4554b21da5c15034605ff23ceb301ac
dlckdgh0414 <ci5976547@gmail.com>
2025-04-22 00:25:21 +0900

    fix : Ghoul

--- Assets/Member/Lch/01 Scripts/Enemy/Enemys/Ghoul/GhoulAttackCompo.cs
+++ Assets/Member/Lch/01 Scripts/Enemy/Enemys/Ghoul/GhoulAttackCompo.cs
@@ -2,8 +2,28 @@ using UnityEngine;
 
 public class GhoulAttackCompo : Attack
 {
+    private Collider _attackCollider;
+    [SerializeField] private float damge;
+    [SerializeField] private Entity _entity;
+    private void Awake()
+    {
+        _attackCollider = GetComponent<Collider>();
+        _attackCollider.enabled = false;
+    }
     public override void EnemyAttack(Transform target, Entity entity)
     {
-        Debug.Log("Attack");
+        _attackCollider.enabled=true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("°ø°ÝÇÒ°í¾ä");
+            if(other.TryGetComponent(out IDamgable damgable))
+            {
+                damgable.ApplyDamage(damge, false, 0, _entity);
+            }
+        }
     }
 }
