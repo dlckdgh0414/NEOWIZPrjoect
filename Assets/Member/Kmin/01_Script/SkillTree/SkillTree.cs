1b23a53751926a8d5780e9c81c26015fbf63379a
rlaals0203 <scientistmin@gmail.com>
2025-05-02 22:54:01 +0900

    refactoring : skilltree

--- Assets/Member/Kmin/01_Script/SkillTree/SkillTree.cs
+++ Assets/Member/Kmin/01_Script/SkillTree/SkillTree.cs
@@ -30,44 +30,64 @@ public class SkillTree : MonoBehaviour
 
     private void HandleFruitsPurchase(SkillTreePurchaseEvent skillTreeEvent)
     {
-        StartCoroutine(ConnectRoutine(skillTreeEvent.SkillTreeNode));
+        ConnectColor(skillTreeEvent.SkillTreeNode);
     }
 
-    public void SelectFruits(NodeSO selectedNode)
+    private void SelectFruits(NodeSO selectedNode)
     {
         _skillTreeEvent.NodeSO = selectedNode;
         eventChannelSO.RaiseEvent(_skillTreeEvent);
     }
 
-    // ReSharper disable Unity.PerformanceAnalysis
-    private IEnumerator ConnectRoutine(SkillTreeNode f)
+    private void ConnectColor(SkillTreeNode f)
     {
         f.transform.SetSiblingIndex(f.ParentNode.transform.GetSiblingIndex() - 1);
-        
-        for (int i = 0; i < 3; i++)
-        {
-            DOTween.To(() => 0, amount => f.FillBranch[i].fillAmount = amount, 1f, 0.2f)
-                .SetEase(Ease.OutQuad);
-            yield return new WaitUntil(() => f.FillBranch[i].fillAmount == 1);
+    
+        Sequence seq = DOTween.Sequence();
+    
+        for (int i = 0; i < 3; i++) {
+            int idx = i;
+            seq.Append(DOTween.To(() => 0f, amount 
+                    => f.FillBranch[idx].fillAmount = amount, 1f,
+                0.2f).SetEase(Ease.OutQuad));
         }
-
-        SetColor(f);
+    
+        seq.OnComplete(() => SetColor(f));
     }
 
     private void SetColor(SkillTreeNode f)
     {
+        Sequence seq = DOTween.Sequence();
         Outline outline = f.GetComponentInChildren<Outline>();
         Color lineColor = outline.effectColor;
-        DOTween.To(() => lineColor, color => outline.effectColor = color, f.branchColor, 1.5f)
-            .SetEase(Ease.InBounce);
         
-        f.NodeIcon.DOColor(f.branchColor, 1f);
-        f.NodeIcon.DOFade(1f, 1f);
-        f.ConnectedNodes.ForEach(f =>
-        {
-            f.NodeIcon.DOColor(Color.white, 1f);
-            f.NodeIcon.DOFade(1f, 1f);
-            f.NodeButton.interactable = true;
-        });
+        DOTween.To(() => lineColor, color => outline.effectColor = color, f.branchColor, 1.5f)
+            .SetEase(Ease.InCubic);
+
+        seq.Join(f.NodeIcon.DOColor(f.branchColor, 1f))
+            .Join(f.NodeIcon.DOFade(1f, 1f))
+            .OnComplete(() =>
+            {
+                f.ConnectedNodes.ForEach(n =>
+                {
+                    n.NodeIcon.DOColor(Color.white, 1f);
+                    n.NodeIcon.DOFade(1f, 1f);
+                    n.NodeButton.interactable = true;
+                });
+            });
+    }
+
+    private void ActiveNodeColor(SkillTreeNode node)
+    {
+        node.NodeIcon.DOColor(Color.white, 1f);
+        node.NodeIcon.DOFade(1f, 1f);
+        node.NodeButton.interactable = true;
+    }
+    
+    private void InActiveNodeColor(SkillTreeNode node)
+    {
+        node.NodeIcon.DOColor(Color.black, 1f);
+        node.NodeIcon.DOFade(0f, 1f);
+        node.NodeButton.interactable = false;
     }
 }
