a565a7804d476da31a3d12740df1c26faf493c32
김민재 <kbckmj2050@gmail.com>
2025-05-10 00:34:39 +0900

    feat : AddChangeRd

--- Assets/Member/Kmj/01.Scipt/Entity/AttackCompo/CharacterMovement.cs
+++ Assets/Member/Kmj/01.Scipt/Entity/AttackCompo/CharacterMovement.cs
@@ -1,4 +1,5 @@
 
+using System;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour, IEntityComponet
@@ -15,6 +16,11 @@ public class CharacterMovement : MonoBehaviour, IEntityComponet
 
     public bool CanManualMovement { get; set; } = true;
 
+    public GameObject Cam1;
+    public GameObject Cam2;
+
+    public bool IsQuarterView { get; set; } = true;
+
     public bool IsRolling { get; set; } = false;
     private Player _entity;
 
@@ -30,6 +36,7 @@ public class CharacterMovement : MonoBehaviour, IEntityComponet
 
     private void Start()
     {
+        Cam2.SetActive(false);
         moveSpeed = _stat.GetStat(_moveSpeedStat).Value;
         rollingSpeed = _stat.GetStat(_rollingSpeedStat).Value;
     }
@@ -41,15 +48,35 @@ public class CharacterMovement : MonoBehaviour, IEntityComponet
         _movementDirection.z = ZMove;
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !IsQuarterView)
+        {
+            IsQuarterView = true;
+            Cam2.SetActive(false);
+            Cam1.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && IsQuarterView)
+        {
+            IsQuarterView = false;
+            Cam1.SetActive(false);
+            Cam2.SetActive(true);
+        }
+    }
+
     private void FixedUpdate()
     {
         CalculateMovement();
+        
+      
 
-        _rbcompo.linearVelocity = new Vector3(transform.TransformDirection(_velocity).x,
-            _rbcompo.linearVelocity.y, transform.TransformDirection(_velocity).z);
-
-
-        //_rbcompo.linearVelocity = new Vector3(_velocity.x, _rbcompo.linearVelocity.y, _velocity.z);
+        if(IsQuarterView)
+            _rbcompo.linearVelocity = new Vector3(_velocity.x, _rbcompo.linearVelocity.y, _velocity.z);
+        else
+        {
+            _rbcompo.linearVelocity = new Vector3(transform.TransformDirection(_velocity).x,
+                _rbcompo.linearVelocity.y, transform.TransformDirection(_velocity).z);
+        }
     }
 
     public void MoveToEntity(Vector3 target)
@@ -71,30 +98,41 @@ public class CharacterMovement : MonoBehaviour, IEntityComponet
         if (CanMove)
         {
 
-            if (CanManualMovement)
+            if (CanManualMovement && !IsQuarterView)
             {
                 _velocity = Quaternion.Euler(0, 0, 0) * _movementDirection;
                 _velocity *= moveSpeed;
             }
+            else if (CanManualMovement && IsQuarterView)
+            {
+                _velocity = Quaternion.Euler(0, -45, 0) * _movementDirection;
+                _velocity *= moveSpeed;
+            }
+            
             else
             {
                 _velocity += _autoMovement * Time.fixedDeltaTime;
             }
 
-            if(IsRolling)
+            if(IsRolling && !IsQuarterView)
             {
                 _velocity = Quaternion.Euler(0, 0f, 0) * _movementDirection;
                 _velocity *= rollingSpeed;
             }
+            else if(IsRolling && IsQuarterView)
+            {
+                _velocity = Quaternion.Euler(0, -45f, 0) * _movementDirection;
+                _velocity *= rollingSpeed;
+            }
 
 
-            /*if (_velocity.magnitude > 0)
+            if (_velocity.magnitude > 0 && IsQuarterView)
             {
                 var targetRotation = Quaternion.LookRotation(_velocity);
                 targetRotation.z = 0;
                 Transform parent = _entity.transform;
                 parent.rotation = Quaternion.Lerp(parent.rotation, targetRotation, Time.fixedDeltaTime * rotationSpeed);
-            }*/
+            }
         }
     }
 
